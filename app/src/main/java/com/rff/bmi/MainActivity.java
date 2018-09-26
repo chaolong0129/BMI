4151f1796457cbf7cf8775f5d9e53dcdfc5f66dc
Robert <chaolong_chen@hotmail.com>
2018-09-26 23:09:44 +0800

    To Use Toast class to show BMI Information.

--- app/src/main/java/com/rff/bmi/MainActivity.java
+++ app/src/main/java/com/rff/bmi/MainActivity.java
@@ -2,12 +2,45 @@ package com.rff.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edweight;
+    private EditText edheight;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findviews();
+    }
+
+    private void findviews() {
+        edweight = findViewById(R.id.edWeight);
+        edheight = findViewById(R.id.edHeight);
+        result = findViewById(R.id.result);
+    }
+
+    public void bmi(View view)
+    {
+        String w = edweight.getText().toString();
+        String h = edheight.getText().toString();
+        float weight = Float.parseFloat(w);
+        float height = Float.parseFloat(h);
+        float bmi = weight/(height*height);
+        Log.d("MainActivity","BMI : " + bmi);
+        Toast.makeText(this, "Your BMI is " + bmi, Toast.LENGTH_LONG).show();
+        result.setText("Your BMI is " + bmi);
+    }
+
+    public void help(View view)
+    {
+
     }
 }
