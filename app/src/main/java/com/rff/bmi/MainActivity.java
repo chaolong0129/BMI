1d2a56932005f75780156cd69ab269366e870776
Robert <chaolong_chen@hotmail.com>
2018-09-27 21:28:39 +0800

    To exercise AlertDialog component and anonymous function to process button behavior.

--- app/src/main/java/com/rff/bmi/MainActivity.java
+++ app/src/main/java/com/rff/bmi/MainActivity.java
@@ -1,9 +1,12 @@
 package com.rff.bmi;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,6 +28,17 @@ public class MainActivity extends AppCompatActivity {
         edweight = findViewById(R.id.edWeight);
         edheight = findViewById(R.id.edHeight);
         result = findViewById(R.id.result);
+        Button btnHelp = findViewById(R.id.btnHelp);
+        btnHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Help")
+                .setMessage("The Body Mass Index (BMI) or Quetelet index is a value derived from the mass (weight) and height of an individual.")
+                .setPositiveButton("OK", null)
+                        .show();
+            }
+        });
     }
 
     public void bmi(View view)
@@ -37,6 +51,17 @@ public class MainActivity extends AppCompatActivity {
         Log.d("MainActivity","BMI : " + bmi);
         Toast.makeText(this, "Your BMI is " + bmi, Toast.LENGTH_LONG).show();
         result.setText("Your BMI is " + bmi);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("BMI")
+                .setMessage("Your BMI is : " + bmi)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        edheight.setText("");
+                        edweight.setText("");
+                    }
+                })
+                .show();
     }
 
     public void help(View view)
